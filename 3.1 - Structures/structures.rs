deddf32add9e4aac08748b090d1e7aba1d34ee9a
AngeloGalav <agalavottib@gmail.com>
2023-11-08 11:54:04 +0100

    Add structs exercise

---
+++ 3.1 - Structures/structures.rs
@@ -0,0 +1,93 @@
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+// A unit struct
+struct Unit;
+
+// A tuple struct
+struct Pair(i32, f32);
+
+#[derive(Debug)]
+// A struct with two fields
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Debug)]
+// Structs can be reused as fields of another struct
+struct Rectangle {
+    // A rectangle can be specified by where the top left and bottom right
+    // corners are in space.
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn square(point: Point, l: f32) -> Rectangle {
+    let new_bottom_left =  Point {x: point.x + l, y: point.y + l};
+    let new = Rectangle {top_left: point, bottom_right: new_bottom_left};
+    new
+}
+
+fn area(rect: Rectangle) -> f32 {
+    let area = (rect.top_left.y - rect.bottom_right.y).abs()*(rect.bottom_right.x - rect.top_left.x).abs();
+    area
+}
+
+fn main() {
+    // Create struct with field init shorthand
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Print debug struct
+    println!("{:?}", peter);
+
+    // Instantiate a `Point`
+    let point: Point = Point { x: 10.3, y: 0.4 };
+
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    // Make a new point by using struct update syntax to use the fields of our
+    // other one
+    let bottom_right = Point { x: 5.2, ..point };
+
+    // `bottom_right.y` will be the same as `point.y` because we used that field
+    // from `point`
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    // Destructure the point using a `let` binding
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
+
+    // Instantiate a unit struct
+    let _unit = Unit;
+
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
+
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+    let square_new = square(point, 2.3);
+    println!("New square\n{:?}", square_new);
+    println!("Area is \n{:?}", area(square_new));
+
+}
